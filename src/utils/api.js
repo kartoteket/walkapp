005ca8a265abbcaa0226a50508fc7a5e04008a69
Svale Fossåskaret <svale@3by5.no>
2017-02-16 14:38:48 +0100

    Add config-argument to api-calls

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,14 +2,14 @@ import axios from 'axios'
 
 export default {
 
-  get: function (url, request) {
-    return axios.get(url, request)
+  get: function (url, request, config) {
+    return axios.get(url, request, config)
     .then((response) => Promise.resolve(response.data))
     .catch((error) => Promise.reject(error))
   },
 
-  post: function (url, request) {
-    return axios.post(url, request)
+  post: function (url, request, config) {
+    return axios.post(url, request, config)
       .then((response) => Promise.resolve(response))
       .catch((error) => Promise.reject(error))
   }
