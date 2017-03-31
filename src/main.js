5a7e219acf521bee0042c46745ccdfe366dbdcf6
Svale Fossåskaret <svale@3by5.no>
2017-03-31 13:47:04 +0200

    Added Raven plugin for error tracking in prod env

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import App from './App'
 import { sync } from 'vuex-router-sync'
 import router from './router'
 import store from './store'
+import Raven from 'raven-js'
+import RavenVue from 'raven-js/plugins/vue'
 import Header from 'components/Header'
 import Footer from 'components/Footer'
 
@@ -14,6 +16,14 @@ Vue.component('footer-component', Footer)
 
 // keep vue-router and vuex store in sync.
 sync(store, router)
+
+// Sentry Raven
+if (process.env.NODE_ENV === 'production') {
+  Raven
+      .config('https://97ee13fdbe364d1388393fa9315aacc3@sentry.io/153958')
+      .addPlugin(RavenVue, Vue)
+      .install()
+}
 
 /* eslint-disable no-new */
 new Vue({
