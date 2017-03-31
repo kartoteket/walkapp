b3bbd6596da7bc89fe0421434ace239e413e4dc2
Svale Fossåskaret <svale@3by5.no>
2017-03-31 15:59:33 +0200

    Implement VueAnalytics

--- src/main.js
+++ src/main.js
@@ -2,20 +2,36 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+
 import { sync } from 'vuex-router-sync'
 import router from './router'
 import store from './store'
+
 import Raven from 'raven-js'
 import RavenVue from 'raven-js/plugins/vue'
+import VueAnalytics from 'vue-analytics'
+
 import Header from 'components/Header'
 import Footer from 'components/Footer'
 
-// gloabl components (Mayb better registered in App.vue and used in that scope/template ???!?)
+// gloabl components (mayb better registered in App.vue and used in that scope/template ???!?)
 Vue.component('header-component', Header)
 Vue.component('footer-component', Footer)
 
 // keep vue-router and vuex store in sync.
 sync(store, router)
+
+//  Google Analytics
+const trackingId = 'UA-72588862-2'
+Vue.use(VueAnalytics, {
+  id: trackingId,
+  router,
+  debug: {
+    enabled: false,
+    trace: false,
+    sendHitTask: true
+  }
+})
 
 // Sentry Raven
 if (process.env.NODE_ENV === 'production') {
