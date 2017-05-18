22224c54516c042a6adf1b6a2b8722ca0083d9ef
Svale Fossåskaret <svale@3by5.no>
2017-05-18 14:58:37 +0200

    Turns on bundleAnalyzer and gzipping

--- config/index.js
+++ config/index.js
@@ -10,18 +10,19 @@ module.exports = {
     assetsRoot: '/Users/svale/Sites/risiko/public_html/walkapp',
     assetsSubDirectory: 'static',
     assetsPublicPath: '/walkapp/',
-    productionSourceMap: true,
+    productionSourceMap: false,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
     // Before setting to `true`, make sure to:
     // npm install --save-dev compression-webpack-plugin
-    productionGzip: false,
+    productionGzip: true,
     productionGzipExtensions: ['js', 'css'],
     // Run the build command with an extra argument to
     // View the bundle analyzer report after build finishes:
     // `npm run build --report`
     // Set to `true` or `false` to always turn it on or off
-    bundleAnalyzerReport: process.env.npm_config_report
+    // bundleAnalyzerReport: process.env.npm_config_report
+    bundleAnalyzerReport: true
   },
   dev: {
     env: require('./dev.env'),
