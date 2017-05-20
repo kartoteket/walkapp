5a6a7d8410d61cba12a576e464fb9c7ff3cf6a25
Svale Fossåskaret <svale@3by5.no>
2017-05-20 09:48:13 +0200

    Add raven to errorHandler
    For capture and optional user feedback dialogue

--- src/utils/errors.js
+++ src/utils/errors.js
@@ -1,11 +1,23 @@
+import Raven from 'raven-js'
 export default {
 
   handler (context, error) {
     // store error message
     context.commit('APP_MESSAGE', {type: 'error', body: error.response.statusText, title: 'Feil!', code: error.response.status})
 
+    // write to console
     if (context.state.config.debug) {
       console.log(error)
+    }
+
+    // capture to sentry
+    if (context.state.config.sentry.capture) {
+      Raven.captureException(error)
+    }
+
+    // Sentry User feeback
+    if (context.state.config.sentry.userFeedback) {
+      Raven.showReportDialog()
     }
   }
 }
