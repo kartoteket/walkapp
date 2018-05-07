78e2a230062293df5e6e443a07224375650d2fb3
Svale Fossåskaret <svale@3by5.no>
2018-05-07 12:04:46 +0200

    Fix config errors

--- static/appConfig.js
+++ static/appConfig.js
@@ -1,10 +1,24 @@
 
-(function (window) {
-  var config = {
-    serverUrl: 'https://myserver.com',
-    foo: 'abc',
+(function(window) {
+  const hostname = window.location.hostname
+  let serverEnv = 'prod'
+  if (hostname.indexOf('.dev') >= 0 || hostname.indexOf('localhost') >= 0) { // process.env.NODE_ENV === 'development' tests dev vs build in npm
+    serverEnv = 'dev'
+  } else if (hostname.indexOf('dev.') >= 0) {
+    serverEnv = 'staging'
+  }
+  const isProduction = serverEnv === 'prod'
+
+  const config = {
+    env: serverEnv,
+    debug: false,
+    apiUrl: '/api',
+    rootUrl: serverEnv === 'dev' ? 'https://risiko.dev' : 'https://' + hostname,
+    itemsSectionId: isProduction ? 7 : 7, // currently in sync, but that may change
+    highlightfirst: false,
+    enableTags: false,
     geoConfig: {
-      enabled: false,  // Todo: Probably best to move this pout of config
+      enabled: false,
       maximumAge: 5 * 60 * 1000,
       timeout: 10 * 1000,
       enableHighAccuracy: true
@@ -15,13 +29,20 @@
       zoomHigh: 20,
       gesture: 'greedy'
     },
-    imageUploader: {
+    imgConfig: {
       debug: 1,
       maxWidth: 512,
       quality: 0.7,
       autoRotate: true,
       outputFormat: 'verbose'
-    }
+    },
+    sentry: {
+      capture: isProduction,
+      logUser: isProduction,
+      userFeedback: isProduction
+    },
   }
+
   window.appConfig = config
-})(window)
+
+})(window);
